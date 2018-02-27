efe5440fb01e091b7500be8973ebdb70553bd55f
beim <beim2015@outlook.com>
2018-02-27 09:56:38 +0800

    1

--- src/routes/caffe.js
+++ src/routes/caffe.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
 
 const models = require('../model')
 const middleware = require('../lib/middleware')
@@ -9,11 +10,12 @@ const caffeUtil = require('../lib/caffeUtil')
 const util = require('../lib/util')
 
 /*
-o POST /caffemodel/:projectId # 上传某个项目的caffemodel
+o POST /caffemodel/:projectId {caffemodel} # 上传某个项目的caffemodel
 x GET /caffe/:projectId {image} # 上传图片调用接口，返回label
 */
 
 router.post('/caffemodel/:projectId', 
+    middleware.validateRequestBody('caffemodel'),
     async (ctx, next) => {
         const params = ctx.params
         const body = ctx.request.body
@@ -25,11 +27,11 @@ router.post('/caffemodel/:projectId',
             projectName = ret.get('name')
         }
         catch (e) {
-            ctx.body = Ret(0, '', e)
+            return ctx.body = Ret(0, '', e)
         }
         const location = path.resolve(__dirname, '../caffeModels', projectName, 'model.caffemodel')
         if (fs.existsSync(location)) fs.unlinkSync(location)
-        util.saveFile(body['buf'], location)
+        util.saveFile(body['caffemodel'], location)
         ctx.body = Ret(1)
         await next()
     })
@@ -39,7 +41,30 @@ router.get('/caffe/:projectId',
     async (ctx, next) => {
         const params = ctx.params
         const body = ctx.request.body
-        
+        const projectId = parseInt(params['projectId'])
+        // 获取caffemodel的路径
+        let projectName = null
+        try {
+            const ret = await models.Project.findById(projectId)
+            if (!ret) throw(`projectId(${projectId}) not found`)
+            projectName = ret.get('name')
+        }
+        catch (e) {
+            return ctx.body = Ret(0, '', e)
+        }
+        const modelLocation = path.resolve(__dirname, '../caffeModels', projectName, 'model.caffemodel')
+        if (!fs.existsSync(modelLocation)) {
+            return ctx.body = Ret(0, `caffemodel(${modelLocation}) not found`)
+        }
+
+        // 保存图片到临时文件
+        const tmpImgPath = path.join(os.tmpdir(), `caffemodel-${new Date().getTime().toString()}`)
+        util.saveFile(body['image'], tmpImgPath)
+
+        ctx.body = Ret(1, '', {
+            modelLocation,
+            tmpImgPath
+        })
         await next()
     })
 
